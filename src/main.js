eba2d6e59f52d42b9762afe70051e396c7444fbd
dumaaas <markodumnic8@gmail.com>
2023-03-17 21:05:40 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './style.css'
+var VueScrollTo = require('vue-scrollto');
+
+Vue.use(VueScrollTo, {
+  container: "#section-container",
+  duration: 700,
+  easing: "ease-in-out",
+  offset: 0,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+})
 
 Vue.config.productionTip = false
 
